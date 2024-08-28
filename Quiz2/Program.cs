5bdf408f31308cc2e6575add0bd798529f071604
ByronWLitle <bwlitle@amarillocollege.com>
2024-08-28 12:59:48 -0500

    Add project files.

---
+++ Quiz2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Quiz2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prompt user for price of item
+            Console.Write("Enter item price: ");
+            decimal price = Convert.ToDecimal(Console.ReadLine());
+            //Prompt user for sales tax in decimal form
+            Console.Write("Enter sales tax (Ex. 0.05 for 5 %): ");
+            decimal tax = Convert.ToDecimal(Console.ReadLine());
+            //Prompts user for discount price in decimal form
+            Console.Write("Enter promotion discount (Ex. 0.05 for 5 %): ");
+            decimal discount = Convert.ToDecimal(Console.ReadLine());
+            //Calculate for total price
+            int discPrice = Convert.ToInt32((price * discount));
+
+            int total = Convert.ToInt32((price- discPrice) * tax);
+            //Display total for user
+            Console.WriteLine($"Your total is ${total}.");
+            Console.ReadLine();
+        }
+    }
+}
